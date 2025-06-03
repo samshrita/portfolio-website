ec729363442d7b8dc12de3cac9d0cd64284997e4
Samshrita ghimire <samshritaghimrie@gmail.com>
2025-06-04 00:34:05 +0545

    Update June 4 2025

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -180,7 +180,7 @@ export default function ProjectList() {
         ],
       },
       demoLink:
-        "https://github.com/samshrita/portfolio-website/releases/tag/APK_File",
+        "https://github.com/samshrita/Monument-Recognition-System/releases/tag/APK_File",
         //change here  regarding releases  https://github.com/Znis/monument-recognition/releases/tag/apkfile 
       videoDemo: "https://www.youtube.com/embed/JQKLWwSyBII",
       projectRoute: RoutingLinks.projects + "/monument-recognition",
